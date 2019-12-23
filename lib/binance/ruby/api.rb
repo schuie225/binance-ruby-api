8da344146746552e485fe770f50526f99ba62db5
Liang Shi <liang.shi@protonmail.com>
2019-12-23 11:39:44 +1100

    first commit

---
+++ lib/binance/ruby/api.rb
@@ -0,0 +1,10 @@
+require "binance/ruby/api/version"
+
+module Binance
+  module Ruby
+    module Api
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/binance/ruby/api/version.rb
@@ -0,0 +1,7 @@
+module Binance
+  module Ruby
+    module Api
+      VERSION = "0.1.0"
+    end
+  end
+end
