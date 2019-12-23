c65884505a062674313d21836a2efc4d5bf121f7
Liang Shi <liang.shi@protonmail.com>
2019-12-24 00:12:34 +1100

    add historical trades

--- lib/binance/market.rb
+++ lib/binance/market.rb
@@ -24,6 +24,33 @@ module Binance
           }
         )
       end
+
+      def trades(symbol:, limit: 500)
+        Client.public_send(
+          path: '/api/v3/trades',
+          params: {
+            symbol: symbol,
+            limit: limit
+          }
+        )
+      end
+
+      def historical_trades(symbol:, limit: 500, from_id: '')
+        # params = {
+        #   symbol: symbol,
+        #   limit: limit,
+        #   from_id: from_id
+        # }.reject(&:empty?)
+
+        Client.public_send(
+          path: '/api/v3/historicalTrades',
+          params: {
+            symbol: symbol,
+            limit: limit,
+            fromId: from_id
+          }.compact
+        )
+      end
     end
   end
 end
